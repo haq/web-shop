21dfdea27a275f9103043686676ce15097133e51
Affan Haq <haqgamer66@gmail.com>
2020-10-23 17:41:37 -0400

    the cart button in navbar is now dynamic

---
+++ app/Http/Livewire/ShowCartButton.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\ShoppingCart;
+use Livewire\Component;
+
+class ShowCartButton extends Component
+{
+    protected $listeners = ['ADDED_PRODUCT_TO_CART' => '$refresh'];
+
+    public function render()
+    {
+        $cart = ShoppingCart::where(
+            [
+                'user_id' => auth()->user()->id,
+                'completed' => false
+            ]
+        )->firstOrFail();
+
+        return view('livewire.show-cart-button', [
+            'count' => $cart->products()->count()
+        ]);
+    }
+}
